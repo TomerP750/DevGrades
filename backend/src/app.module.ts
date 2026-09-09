c80bf3777a2e1f6fc0fb21931e3bd6e3bec6faf4
TomerP750 <tomer.jcb65@gmail.com>
2026-09-09 16:14:32 +0300

    update gitignore

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [UsersModule, AuthenticationModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env.development', // add cross env 
+  })],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_PIPE,
