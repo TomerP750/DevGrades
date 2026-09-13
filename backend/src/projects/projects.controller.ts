233cedfa5ad4edea46c7713a9c4f7704bfc71674
TomerP750 <tomer.jcb65@gmail.com>
2026-09-13 12:17:27 +0300

    Update projects.controller.ts

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { SerializePage } from '../shared/interceptors/serialize.interceptor';
 import { CursorPaginationQueryDto } from '../shared/pagination/cursor-pagination-query.dto';
 import { CursorPaginatedResult } from '../shared/pagination/cursor-pagination.types';
 import { ProjectDto } from './dto/project.dto';
 import { Project } from './entities/project.entity';
 import { ProjectsService } from './projects.service';
+import { CurrentUserId } from '../authentication/decorators/current-user.decorator';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Controller('/api/projects')
 export class ProjectsController {
@@ -14,6 +17,21 @@ export class ProjectsController {
   @SerializePage(ProjectDto)
   async allProjects(@Query() query: CursorPaginationQueryDto): Promise<CursorPaginatedResult<Project>> {
     return this.projectsService.findAll(query);
+  }
+
+  @Post("/create")
+  async createProject(@CurrentUserId() userId: string, @Body() createProjectDto: CreateProjectDto) {
+    this.projectsService.create(userId, createProjectDto);
+  }
+
+  @Put(":projectId")
+  async updateProject(@CurrentUserId() userId: string, @Param("projectId") projectId: string, @Body() updateProjectDto: UpdateProjectDto) {
+    this.projectsService.update(userId, projectId, updateProjectDto);
+  }
+
+  @Delete(":projectId")
+  async deleteProject(@CurrentUserId() userId: string, @Param("projectId") projectId: string): Promise<void> {
+    this.projectsService.delete(userId, projectId);
   }
 
   
